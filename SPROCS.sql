b6ee2e017365c061c4f1a957d208f44f4316955e
mmtsweng <mmtswng@gmail.com>
2018-11-05 05:19:46 -0800

    2018 Initial Setup

--- SPROCS.sql
+++ SPROCS.sql
@@ -1,13 +1,13 @@
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_FINISHERS`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_FINISHERS`()
     NO SQL
 SELECT r.ID, rs.RacerNumber, rs.SwimmerID, s.FirstName,
-	s.LastName, s.City, s.Country, r.Description,
+    s.LastName, s.City, s.Country, r.Description,
     r.Cap, r.CapHex, rs.HasFins,
-    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime))) 
+    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime)))
     AS averageDate, tr.StartTime
-FROM Races r, RaceSwimmers rs, Swimmers s, 
-	TimeSwimmer ts, TimeRace tr
+FROM Races r, RaceSwimmers rs, Swimmers s,
+    TimeSwimmer ts, TimeRace tr
 WHERE rs.SwimmerID = s.ID
 AND rs.RaceID = r.ID
 AND tr.RaceID = r.ID
@@ -17,15 +17,15 @@ ORDER BY averageDate$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_FINISHORDER`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_FINISHORDER`()
     NO SQL
 SELECT r.ID, rs.RacerNumber, rs.SwimmerID, s.FirstName,
-	s.LastName, s.Gender, s.Birthdate, s.City,
+    s.LastName, s.Gender, s.Birthdate, s.City,
     s.Country,r.Description, r.Cap, r.CapHex, rs.HasFins,
-    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime))) 
+    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime)))
     AS averageDate, tr.StartTime
-FROM Races r, RaceSwimmers rs, Swimmers s, 
-	TimeSwimmer ts, TimeRace tr
+FROM Races r, RaceSwimmers rs, Swimmers s,
+    TimeSwimmer ts, TimeRace tr
 WHERE rs.SwimmerID = s.ID
 AND rs.RaceID = r.ID
 AND tr.RaceID = r.ID
@@ -35,96 +35,96 @@ ORDER BY averageDate$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_NONRACERS`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_NONRACERS`()
     NO SQL
 SELECT s.ID, s.FirstName, s.LastName
 FROM Swimmers s
 WHERE s.ID NOT IN (
-	SELECT SwimmerID FROM RaceSwimmers) 
-	ORDER BY s.LastName LIMIT 0, 300$$
+    SELECT SwimmerID FROM RaceSwimmers)
+    ORDER BY s.LastName LIMIT 0, 300$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_RACES`()
-SELECT r.ID, r.Description, r.CapHex, r.Cap 
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_RACES`()
+SELECT r.ID, r.Description, r.CapHex, r.Cap
 FROM Races r$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_RACEFINISHERCOUNTS`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_RACEFINISHERCOUNTS`()
     NO SQL
 SELECT COUNT(DISTINCT rs.SwimmerID) as Swimmers,
-	COUNT(DISTINCT ts.RaceSwimmerID) as Finishers,
-	r.Description, r.CapHex
+    COUNT(DISTINCT ts.RaceSwimmerID) as Finishers,
+    r.Description, r.CapHex
 FROM RaceSwimmers rs
 LEFT JOIN TimeSwimmer ts ON ts.RaceSwimmerID = rs.ID
-	JOIN Races r on rs.RaceID = r.ID
+    JOIN Races r on rs.RaceID = r.ID
 GROUP BY rs.RaceID
 UNION
 SELECT COUNT(DISTINCT rs.SwimmerID) as Swimmers,
-	COUNT(DISTINCT ts.RaceSwimmerID) as Finishers,
-	"Overall", "#008080"
+    COUNT(DISTINCT ts.RaceSwimmerID) as Finishers,
+    "Overall", "#008080"
 FROM RaceSwimmers rs
 LEFT JOIN TimeSwimmer ts ON ts.RaceSwimmerID = rs.ID
-	JOIN Races r on rs.RaceID = r.ID$$
+    JOIN Races r on rs.RaceID = r.ID$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_RACETIMES`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_RACETIMES`()
     NO SQL
 SELECT r.ID, r.Description, r.CapHex, t.StartTime
-FROM Races r 
+FROM Races r
 LEFT JOIN TimeRace t ON r.ID = t.RaceID
 AND t.IsEnabled = 1$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_SWIMMERS`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_SWIMMERS`()
     NO SQL
-SELECT ID, FirstName, LastName, Gender, Birthdate 
+SELECT ID, FirstName, LastName, Gender, Birthdate
 FROM Swimmers$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `INSERT_RACER`(IN `racernumber` INT, IN `swimmerid` INT, IN `raceid` INT, IN `hasfins` INT)
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `INSERT_RACER`(IN `racernumber` INT, IN `swimmerid` INT, IN `raceid` INT, IN `hasfins` INT)
     NO SQL
-INSERT INTO RaceSwimmers 
-	(SwimmerID, RaceID, RacerNumber, HasFins)
-VALUES 
-	(swimmerid, raceid, racernumber, hasfins)$$
+INSERT INTO RaceSwimmers
+    (SwimmerID, RaceID, RacerNumber, HasFins)
+VALUES
+    (swimmerid, raceid, racernumber, hasfins)$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_SWIMMER`(IN `in_swimmerid` INT)
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_SWIMMER`(IN `in_swimmerid` INT)
     NO SQL
-SELECT ID, FirstName, LastName, City, 
-	Country, Birthdate, Gender
+SELECT ID, FirstName, LastName, City,
+    Country, Birthdate, Gender
 FROM Swimmers s
 WHERE s.ID = in_swimmerid$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `INSERT_SWIMMER`(IN `in_firstname` VARCHAR(255), IN `in_lastname` VARCHAR(255), IN `in_city` VARCHAR(64), IN `in_country` VARCHAR(64), IN `in_birthdate` DATE, IN `in_gender` VARCHAR(4))
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `INSERT_SWIMMER`(IN `in_firstname` VARCHAR(255), IN `in_lastname` VARCHAR(255), IN `in_city` VARCHAR(64), IN `in_country` VARCHAR(64), IN `in_birthdate` DATE, IN `in_gender` VARCHAR(4))
     NO SQL
-Insert INTO Swimmers (FirstName, LastName, Gender, 
-		Birthdate, City, Country)
+Insert INTO Swimmers (FirstName, LastName, Gender,
+        Birthdate, City, Country)
 VALUES (in_firstname, in_lastname, in_gender,
         in_birthdate, in_city, in_country)$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `REPORT_OVERALL`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `REPORT_OVERALL`()
     NO SQL
-SELECT r.ID, rs.RacerNumber, rs.SwimmerID, 
-	s.FirstName, s.LastName, s.Gender, s.Birthdate,
-	s.City, s.Country, 
+SELECT r.ID, rs.RacerNumber, rs.SwimmerID,
+    s.FirstName, s.LastName, s.Gender, s.Birthdate,
+    s.City, s.Country,
     TIMESTAMPDIFF(YEAR, s.Birthdate, NOW()) as Age,
-	FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime))) 
-    	AS EndTime, tr.StartTime
-FROM Races r, RaceSwimmers rs, Swimmers s, 
-	TimeSwimmer ts, TimeRace tr
+    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime)))
+        AS EndTime, tr.StartTime
+FROM Races r, RaceSwimmers rs, Swimmers s,
+    TimeSwimmer ts, TimeRace tr
 WHERE rs.SwimmerID = s.ID
-	AND rs.RaceID = r.ID
+    AND rs.RaceID = r.ID
     AND tr.RaceID = r.ID
     AND ts.RaceSwimmerID = rs.ID
     GROUP BY ts.RaceSwimmerID
@@ -133,16 +133,16 @@ LIMIT 5$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_RACEFINISHERS`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_RACEFINISHERS`()
     NO SQL
 SELECT r.ID, rs.RacerNumber, rs.SwimmerID, s.FirstName,
-	s.LastName, s.City, s.Country, r.Description,
+    s.LastName, s.City, s.Country, r.Description,
     r.Cap, r.CapHex, rs.HasFins,
-    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime))) 
+    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime)))
     AS averageDate,
     tr.StartTime
-FROM Races r, RaceSwimmers rs, Swimmers s, 
-	TimeSwimmer ts, TimeRace tr
+FROM Races r, RaceSwimmers rs, Swimmers s,
+    TimeSwimmer ts, TimeRace tr
 WHERE rs.SwimmerID = s.ID
 AND rs.RaceID = r.ID
 AND tr.RaceID = r.ID
@@ -152,73 +152,73 @@ ORDER BY rs.RaceID, averageDate$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `INSERT_TIMERACE`(IN `in_race` INT, IN `in_date` DATETIME, IN `in_enable` INT)
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `INSERT_TIMERACE`(IN `in_race` INT, IN `in_date` DATETIME, IN `in_enable` INT)
     NO SQL
 INSERT INTO TimeRace (RaceID, StartTime, IsEnabled)
-	VALUES (in_race, in_date, in_enable)$$
+    VALUES (in_race, in_date, in_enable)$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `REPORT_OVERALL_19OVER`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `REPORT_OVERALL_19OVER`()
     NO SQL
-SELECT r.ID, rs.RacerNumber, rs.SwimmerID, 
-	s.FirstName, s.LastName, s.Gender, s.Birthdate,
-	s.City, s.Country, 
+SELECT r.ID, rs.RacerNumber, rs.SwimmerID,
+    s.FirstName, s.LastName, s.Gender, s.Birthdate,
+    s.City, s.Country,
     TIMESTAMPDIFF(YEAR, s.Birthdate, NOW()) as Age,
-	FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime))) 
-    	AS EndTime, 
+    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime)))
+        AS EndTime,
     tr.StartTime
-FROM Races r, RaceSwimmers rs, Swimmers s, 
-	TimeSwimmer ts, TimeRace tr
+FROM Races r, RaceSwimmers rs, Swimmers s,
+    TimeSwimmer ts, TimeRace tr
 WHERE rs.SwimmerID = s.ID
     AND rs.RaceID = r.ID
     AND tr.RaceID = r.ID
     AND ts.RaceSwimmerID = rs.ID
     AND rs.HasFins = 0
-    AND TIMESTAMPDIFF(YEAR, s.Birthdate, 
-                      NOW()) BETWEEN 19 AND 999 
+    AND TIMESTAMPDIFF(YEAR, s.Birthdate,
+                      NOW()) BETWEEN 19 AND 999
     GROUP BY ts.RaceSwimmerID
     ORDER BY EndTime
     LIMIT 5$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `REPORT_WINNERS_BY_RACE_AGE`(IN `in_raceid` INT, IN `in_minage` INT, IN `in_maxage` INT)
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `REPORT_WINNERS_BY_RACE_AGE`(IN `in_raceid` INT, IN `in_minage` INT, IN `in_maxage` INT)
     NO SQL
-SELECT r.ID, rs.RacerNumber, rs.SwimmerID, 
-	s.FirstName, s.LastName, s.Gender, s.Birthdate,
-	s.City, s.Country, 
+SELECT r.ID, rs.RacerNumber, rs.SwimmerID,
+    s.FirstName, s.LastName, s.Gender, s.Birthdate,
+    s.City, s.Country,
     TIMESTAMPDIFF(YEAR, s.Birthdate, NOW()) as Age,
-	FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime))) 
-    	AS EndTime, 
+    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime)))
+        AS EndTime,
     tr.StartTime
-FROM Races r, RaceSwimmers rs, Swimmers s, 
-	TimeSwimmer ts, TimeRace tr
+FROM Races r, RaceSwimmers rs, Swimmers s,
+    TimeSwimmer ts, TimeRace tr
 WHERE rs.SwimmerID = s.ID
     AND rs.RaceID = r.ID
     AND tr.RaceID = r.ID
     AND r.ID = in_raceid
     AND ts.RaceSwimmerID = rs.ID
     AND rs.HasFins = 0
-    AND TIMESTAMPDIFF(YEAR, s.Birthdate, 
-                      NOW()) BETWEEN in_minage AND in_maxage 
+    AND TIMESTAMPDIFF(YEAR, s.Birthdate,
+                      NOW()) BETWEEN in_minage AND in_maxage
     GROUP BY ts.RaceSwimmerID
     ORDER BY EndTime
     LIMIT 3$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `REPORT_OVERALL_FIN`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `REPORT_OVERALL_FIN`()
     NO SQL
-SELECT r.ID, rs.RacerNumber, rs.SwimmerID, 
-	s.FirstName, s.LastName, s.Gender, s.Birthdate,
-	s.City, s.Country, 
+SELECT r.ID, rs.RacerNumber, rs.SwimmerID,
+    s.FirstName, s.LastName, s.Gender, s.Birthdate,
+    s.City, s.Country,
     TIMESTAMPDIFF(YEAR, s.Birthdate, NOW()) as Age,
-	FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime))) 
-    	AS EndTime, 
+    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime)))
+        AS EndTime,
     tr.StartTime
-FROM Races r, RaceSwimmers rs, Swimmers s, 
-	TimeSwimmer ts, TimeRace tr
+FROM Races r, RaceSwimmers rs, Swimmers s,
+    TimeSwimmer ts, TimeRace tr
 WHERE rs.SwimmerID = s.ID
     AND rs.RaceID = r.ID
     AND tr.RaceID = r.ID
@@ -230,7 +230,7 @@ WHERE rs.SwimmerID = s.ID
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `SP_FINISHRACER`(IN `in_racernumber` INT, IN `in_finishtime` DATETIME)
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `SP_FINISHRACER`(IN `in_racernumber` INT, IN `in_finishtime` DATETIME)
     NO SQL
 INSERT INTO TimeSwimmer (RaceSwimmerID, FinishTime, IsEnabled)
 VALUES (
@@ -242,62 +242,62 @@ VALUES (
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `SP_RESETTIMERACE`(IN `in_race` INT)
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `SP_RESETTIMERACE`(IN `in_race` INT)
     NO SQL
-UPDATE TimeRace 
-	SET IsEnabled = 0 
+UPDATE TimeRace
+    SET IsEnabled = 0
     WHERE RaceID = in_race$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_RACERS`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_RACERS`()
     NO SQL
 SELECT rs.ID, rs.RacerNumber, rs.SwimmerID, s.FirstName,
-	s.LastName,s.Gender, s.Birthdate, s.City, s.Country,
+    s.LastName,s.Gender, s.Birthdate, s.City, s.Country,
     r.Description, r.Cap, rs.HasFins
-FROM RaceSwimmers rs, Races r, Swimmers s 
-WHERE rs.SwimmerID = s.ID 
-AND rs.RaceID = r.ID 
+FROM RaceSwimmers rs, Races r, Swimmers s
+WHERE rs.SwimmerID = s.ID
+AND rs.RaceID = r.ID
 ORDER BY s.LastName, s.FirstName$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `REPORT_OVERALL_UNDER19`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `REPORT_OVERALL_UNDER19`()
     NO SQL
-SELECT r.ID, rs.RacerNumber, rs.SwimmerID, 
-	s.FirstName, s.LastName, s.Gender, s.Birthdate,
-	s.City, s.Country, 
+SELECT r.ID, rs.RacerNumber, rs.SwimmerID,
+    s.FirstName, s.LastName, s.Gender, s.Birthdate,
+    s.City, s.Country,
     TIMESTAMPDIFF(YEAR, s.Birthdate, NOW()) as Age,
-	FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime))) 
-    	AS EndTime, 
+    FROM_UNIXTIME(AVG(UNIX_TIMESTAMP(ts.FinishTime)))
+        AS EndTime,
     tr.StartTime
-FROM Races r, RaceSwimmers rs, Swimmers s, 
-	TimeSwimmer ts, TimeRace tr
+FROM Races r, RaceSwimmers rs, Swimmers s,
+    TimeSwimmer ts, TimeRace tr
 WHERE rs.SwimmerID = s.ID
     AND rs.RaceID = r.ID
     AND tr.RaceID = r.ID
     AND ts.RaceSwimmerID = rs.ID
     AND rs.HasFins = 0
-    AND TIMESTAMPDIFF(YEAR, s.Birthdate, 
-                      NOW()) BETWEEN 0 AND 18 
+    AND TIMESTAMPDIFF(YEAR, s.Birthdate,
+                      NOW()) BETWEEN 0 AND 18
     GROUP BY ts.RaceSwimmerID
     ORDER BY EndTime
     LIMIT 5$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `UPDATE_SWIMMER`(IN `in_swimmerid` INT, IN `in_city` VARCHAR(50), IN `in_country` VARCHAR(64), IN `in_birthdate` DATE, IN `in_firstname` VARCHAR(255), IN `in_lastname` VARCHAR(255), IN `in_gender` VARCHAR(4))
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `UPDATE_SWIMMER`(IN `in_swimmerid` INT, IN `in_city` VARCHAR(50), IN `in_country` VARCHAR(64), IN `in_birthdate` DATE, IN `in_firstname` VARCHAR(255), IN `in_lastname` VARCHAR(255), IN `in_gender` VARCHAR(4))
     NO SQL
-UPDATE Swimmers 
+UPDATE Swimmers
 SET Gender=in_gender,
-	Birthdate=in_birthdate,
+    Birthdate=in_birthdate,
     City=in_city,
-	Country=in_country
+    Country=in_country
 WHERE ID=in_swimmerid$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `GET_RACERCOUNTS`()
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `GET_RACERCOUNTS`()
     NO SQL
 SELECT r.ID, r.Description, COUNT(rs.ID) as Swimmers
 FROM RaceSwimmers AS rs, Races r
@@ -306,12 +306,12 @@ GROUP BY r.ID$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE DEFINER=`phpmyadmin`@`localhost` PROCEDURE `UPDATE_RACESWIMMERS`(IN `in_raceswimmerid` INT, IN `in_swimmerid` INT, IN `in_raceid` INT, IN `in_racernumber` INT, IN `in_hasfins` INT)
+CREATE DEFINER=`swimrace`@`localhost` PROCEDURE `UPDATE_RACESWIMMERS`(IN `in_raceswimmerid` INT, IN `in_swimmerid` INT, IN `in_raceid` INT, IN `in_racernumber` INT, IN `in_hasfins` INT)
     NO SQL
-UPDATE RaceSwimmers 
+UPDATE RaceSwimmers
 SET SwimmerID=in_swimmerid,
-	RaceID=in_raceid,
-	RacerNumber=in_racernumber,
-	HasFins=in_hasfins
+    RaceID=in_raceid,
+    RacerNumber=in_racernumber,
+    HasFins=in_hasfins
 WHERE ID=in_raceswimmerid$$
 DELIMITER ;
--- api.interface.php
+++ api.interface.php
@@ -1,60 +1,60 @@
 <?php
-	require_once("rest.inc.php");
-	
+    require_once("rest.inc.php");
+
 class APIInterface extends REST
 {
-	public $data = "";
-	
-	//Constructor
-	public function __construct()
-	{
-		parent::__construct();
-	}
-	
-	//Connect to Database
-	private function dbConnect()
-	{
-		$mysqli  = mysqli_connect("localhost", "swimrace", "stcroix", "Swimrace");
-		if ($mysqli->connect_errno){
-			error_log($mysqli->connect_error);
-		}
-		return $mysqli;
-	}
-	
-	private function dbDisconnect($mysqli)
-	{
-		mysqli_close($mysqli);
-	}
-	
-	
-	// Method to get a JSON response to a stored procedure
-	public function get_sproc($sprocname)
-	{
-		error_log("Calling :" . $sprocname);
-		$mysqli = $this->dbConnect();
-		$result = mysqli_query($mysqli,"CALL " . $sprocname) 
-			or die("Query Fail: " . mysqli_error());
-			
-		$data = array();
-		while ($row = mysqli_fetch_assoc($result))
-		{
-			$data[] = $row;
-		}
-		
-		$this->dbDisconnect($mysqli);
-		
-		return $data;
-	}
-	
-	public function call_sproc($sprocname)
-	{
-		error_log("Calling :" . $sprocname);
-		$mysqli = $this->dbConnect();
-		$result = mysqli_query($mysqli,"CALL " . $sprocname) 
-			or die("Query Fail: " . mysqli_error());
-		
-		$this->dbDisconnect($mysqli);
-		return $result;		
-	}	
+    public $data = "";
+
+    //Constructor
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    //Connect to Database
+    private function dbConnect()
+    {
+        $mysqli  = mysqli_connect("localhost", "swimrace", "swimvast", "Swimrace");
+        if ($mysqli->connect_errno){
+            error_log($mysqli->connect_error);
+        }
+        return $mysqli;
+    }
+
+    private function dbDisconnect($mysqli)
+    {
+        mysqli_close($mysqli);
+    }
+
+
+    // Method to get a JSON response to a stored procedure
+    public function get_sproc($sprocname)
+    {
+        error_log("Calling :" . $sprocname);
+        $mysqli = $this->dbConnect();
+        $result = mysqli_query($mysqli,"CALL " . $sprocname)
+            or die("Query Fail: " . mysqli_error($mysqli));
+
+        $data = array();
+        while ($row = mysqli_fetch_assoc($result))
+        {
+            $data[] = $row;
+        }
+
+        $this->dbDisconnect($mysqli);
+
+        return $data;
+    }
+
+    public function call_sproc($sprocname)
+    {
+        error_log("Calling :" . $sprocname);
+        $mysqli = $this->dbConnect();
+        $result = mysqli_query($mysqli,"CALL " . $sprocname)
+            or die("Query Fail: " . mysqli_error($mysqli));
+
+        $this->dbDisconnect($mysqli);
+        return $result;
+    }
 }
 ?>
